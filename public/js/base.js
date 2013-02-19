83503c4c6b631b470cb2e341fb33ff7f6fb44f44
Onur Baysan <onurbaysan@gmail.com>
2013-02-19 02:26:17 +0200

    First commit

---
+++ public/js/base.js
@@ -0,0 +1,111 @@
+var socket;
+var nck;
+
+function clientSocket(nickname) {
+	
+	socket = io.connect('http://localhost:3000');
+		
+	socket.on("connect", function() {
+		socket.emit("newUser", nickname);
+    });
+	
+	
+	// Send message
+	socket.on("sendMessage" , function(nickname, data) {		
+		var cls = $('#firstone').attr('class');
+		
+		
+		if(nickname == 'System') {
+			$('#general').append("<p class='text-success'>" + nickname + " : " + data + "</p>");	
+			$('#General-span').text(parseInt($('#General-span').text())+1);			
+		} else {
+			$('#general').append("<p class='muted'>" + nickname + " : " + data + "</p>");	
+			$('#General-span').text(parseInt($('#General-span').text())+1);
+		}		
+		
+		if(cls == 'active') {
+			$('#General-span').text(0);
+			$('#General-span').hide();
+		} else
+			$('#General-span').show();
+		
+	});
+	
+	// Retrieve user list
+    socket.on("refreshUsers", function(data){ 
+		$('#userlist').find('li:not(:first)').remove();		
+		$('#tabcontent').find('div:not(:first)').remove();
+		/* $('#userlist').append("<li class='active'><a href='#general' data-toggle='tab' id='General'>General</a></li>");
+		$('#tabcontent').append("<div id='general' class='tab-pane active'><h4>Messages</h4></div>"); */
+		
+        $.each(data, function(key, value){		
+			if(key != nck) {
+				$('#userlist').append("<li id='" + key +"-li'><a href='#" + key + "' data-toggle='tab' id='" + key +"-anc'>" + key + "<span style='display: none' class='badge badge-important' id='"+ key + "-span'>0</span></a></li>");
+				$('#tabcontent').append("<div id='" + key + "' class='tab-pane'><h4>Private Messages Between "+ nck + " and " + key +"</h4><span class='label label-info' id='" + key + "-status'></span></div>");				
+			}
+        }); 
+    });		
+	
+	
+	// Private Messaging
+    socket.on("privateMessage", function(nickname, data){
+		var cls = $('#' + nickname + '-li').attr('class');
+		
+        $('#' + nickname).append("<p><font color='red'>" + nickname + "</font> : " + data + "</p>");		
+			
+		
+		//Increase notification count 
+		$('#' + nickname + '-span').text(parseInt($('#' + nickname + '-span').text())+1);	
+		
+		if(cls == 'active') {
+			$('#' + nickname + '-span').text(0);
+			$('#' + nickname + '-span').hide();
+			socket.emit('updateStatus', { msg: "[[[SEEN]]]"  , to: nickname , from: nck });	
+		} else
+			$('#' + nickname + '-span').show(); 
+    });
+	
+	socket.on("updateStatus" , function(nickname, data) 
+	{
+		$('#' + nickname + '-status').text(data);
+	});
+	
+	socket.on("disconnect", function(){});
+}
+
+function test() {
+	nck = $('#nickname').val();
+	$('#loginScreen').fadeOut("slow", function(){
+        $('#chatScreen').fadeIn("slow");
+    });	
+	document.title = nck;		
+	clientSocket(nck);
+}
+
+function message() {
+	
+	var txt = $('#textMessage').val();
+	var to = $('#userlist li.active').find('a').attr('id');	
+	to = to.substring(0,to.indexOf('-anc'));	
+	
+	$('#textMessage').val('');
+	
+	if(to == 'General') 
+	{
+		$('#general').append("<p class='text-warning'>" + nck + " : " + txt + "</p>");	
+		socket.emit('sendMessage', nck, txt);
+	} else {		
+		$('#' + to).append("<p class='text-warning'>" + nck + " : " + txt + "</p>");		
+		socket.emit('privateMessage', { msg: txt, to: to , from: nck });	
+	}	
+}
+
+function updateStat(message) {
+	var to = $('#userlist li.active').find('a').attr('id');	
+	to = to.substring(0,to.indexOf('-anc'));
+	
+	if(to != 'General')
+	{
+		socket.emit('updateStatus', { msg: message , to: to , from: nck });	
+	}
+}
---
+++ server.js
@@ -0,0 +1,82 @@
+var http = require('http'),
+    fs = require('fs'),
+    // NEVER use a Sync function except at start-up!
+    index = fs.readFileSync(__dirname + '/index.html');
+
+// Send index.html to all requests
+var app = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(index);
+});
+
+
+// Socket.io server listens to our app
+var io = require('socket.io').listen(app);
+
+
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+
+app.listen(3000);
+
+// Connected user list
+var users = {};
+// Socket infos of connected users 
+var sockets = {};
+
+// Emit welcome message on connection
+io.sockets.on('connection', function(socket) {	
+	socket.on("newUser" , function(username) {
+		socket.username = username;		
+		socket.userId = users.length;
+		
+		users[username] = {
+			username : username,
+			userId : users.length
+		};
+			
+		sockets[username] = socket;
+
+		// Send only the connected user
+		socket.emit("sendMessage" , "System" , "Welcome...");
+			
+		// Send message all users except the last connected user
+		socket.broadcast.emit("sendMessage", "System" , username + " is connected");
+					
+		io.sockets.emit("refreshUsers" , users);
+		
+	});
+
+	socket.on('disconnect', function(){		        
+        // Broadcast disconnected users name
+        socket.broadcast.emit("sendMessage", "System", socket.username + " is disconnected :(");
+		
+		delete sockets[socket.username];		
+		delete users[socket.username]; 
+		
+        // Send connected user list to the connected user
+        io.sockets.emit("refreshUsers", users);
+    });	
+	
+	// Client tarafýndan mesaj yollama fonksiyonu
+    socket.on("sendMessage", function(nickname, data){
+        // Baðlý kullanýcýlara kullanýcýdan gelen mesajý yolluyoruz
+        //io.sockets.emit("sendMessage", nickname, data);
+		socket.broadcast.emit("sendMessage", nickname, data);
+    });
+	
+	socket.on("privateMessage" , function(data) {
+		sockets[data.to].emit("privateMessage", data.from, data.msg);		
+	});
+	
+	socket.on("updateStatus" , function(data) 
+	{
+		if(data.msg === '[[[SEEN]]]')
+		{
+			sockets[data.to].emit("updateStatus", data.from, "Seen at: " + new Date().toUTCString());	
+		} else {
+			sockets[data.to].emit("updateStatus", data.from, data.msg);	
+		}
+	});
+});
